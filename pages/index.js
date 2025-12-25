b939c58103715cde14459af8fd5d40d8444f05e1
imantoashwin <sit22it119@sairamtap.edu.in>
2025-12-25 19:41:27 +0530

    Homecomponent-2 Done

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,13 @@
-import { useContext, useState } from "react";
-import ConnectTogether from "../components/ConnectTogether/ConnectTogether";
-import FarmingContent from "../components/FarmingContent/FarmingContent";
-import FarmToHome from "../components/FarmToHome/FarmToHome";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
-import NewsFeed from "../components/NewsFeed/NewsFeed";
-import OrdersLocation from "../components/OrdersLocation/OrdersLocation";
-import { getCookie, hasCookie, setCookie } from "cookies-next";
-import AdminPanel from "../components/AdminPanel/adminPanel";
 import HomeComponent1 from "../components/HomeComponent/HomeComponent1";
+import HomeComponent2 from "../components/HomeComponent/HomeComponent2";
 export default function Home() {
   return (
     <div className="">
       <Navbar />
       <HomeComponent1 />
+      <HomeComponent2 />
       <Footer />
     </div>
     // <AdminPanel/>
